ac50b86a1f0447a58053019672d1891a48be4e4e
Ed Kelly <edwardjkelly@gmail.com>
2021-10-23 22:36:32 +0100

    Improve validators

--- src/Field.elm
+++ src/Field.elm
@@ -2,6 +2,7 @@ module Field exposing (..)
 
 import Element exposing (..)
 import Element.Background as Background
+import Element.Font as Font
 import Element.Input as Input
 import Internals exposing (..)
 
@@ -9,13 +10,44 @@ import Internals exposing (..)
 type Error
     = NotValidInt
     | NotValidFloat
-    | IntTooLow
-    | IntTooHigh
-    | FloatTooLow
-    | FloatTooHigh
-    | StringTooShort
-    | StringTooLong
+    | IntTooLow Int
+    | IntTooHigh Int
+    | FloatTooLow Float
+    | FloatTooHigh Float
+    | StringTooShort Int
+    | StringTooLong Int
     | StringMustNotContain String
+
+
+errorToString : Error -> String
+errorToString e =
+    case e of
+        NotValidInt ->
+            "Must be a whole number"
+
+        NotValidFloat ->
+            "Must be a number"
+
+        IntTooLow i ->
+            "Must be a whole number higher than " ++ String.fromInt i
+
+        IntTooHigh i ->
+            "Must be a whole number lower than " ++ String.fromInt i
+
+        FloatTooLow f ->
+            "Must be a number higher than " ++ String.fromFloat f
+
+        FloatTooHigh f ->
+            "Must be a number lower than " ++ String.fromFloat f
+
+        StringTooShort i ->
+            "Must be longer than " ++ String.fromInt i ++ " characters"
+
+        StringTooLong i ->
+            "Must be shorter than " ++ String.fromInt i ++ " characters"
+
+        StringMustNotContain str ->
+            "Must not contain '" ++ str ++ "'"
 
 
 type Field input output msg
@@ -98,13 +130,13 @@ ifTrueThenJust a bool =
 stringMustBeLongerThan : Int -> String -> Maybe Error
 stringMustBeLongerThan numberOfChars str =
     (String.length str <= numberOfChars)
-        |> ifTrueThenJust StringTooShort
+        |> ifTrueThenJust (StringTooShort numberOfChars)
 
 
 stringMustBeShorterThan : Int -> String -> Maybe Error
 stringMustBeShorterThan numberOfChars str =
     (String.length str >= numberOfChars)
-        |> ifTrueThenJust StringTooLong
+        |> ifTrueThenJust (StringTooLong numberOfChars)
 
 
 stringMustNotContain : String -> String -> Maybe Error
@@ -116,44 +148,52 @@ stringMustNotContain content str =
 intMustBeGreaterThan : Int -> Int -> Maybe Error
 intMustBeGreaterThan tooLow int_ =
     (int_ <= tooLow)
-        |> ifTrueThenJust IntTooLow
+        |> ifTrueThenJust (IntTooLow tooLow)
 
 
 intMustBeLessThan : Int -> Int -> Maybe Error
 intMustBeLessThan tooHigh int_ =
     (int_ >= tooHigh)
-        |> ifTrueThenJust IntTooHigh
+        |> ifTrueThenJust (IntTooHigh tooHigh)
 
 
 floatMustBeGreaterThan : Float -> Float -> Maybe Error
 floatMustBeGreaterThan tooLow float_ =
     (float_ <= tooLow)
-        |> ifTrueThenJust FloatTooLow
+        |> ifTrueThenJust (FloatTooLow tooLow)
 
 
 floatMustBeLessThan : Float -> Float -> Maybe Error
 floatMustBeLessThan tooHigh float_ =
     (float_ >= tooHigh)
-        |> ifTrueThenJust FloatTooHigh
+        |> ifTrueThenJust (FloatTooHigh tooHigh)
 
 
 toEl_ : String -> Bool -> (String -> msg) -> Result (List Error) output -> Element msg
 toEl_ input touched msg res =
-    Input.text
-        [ Background.color
-            (case ( touched, res ) of
-                ( True, Ok _ ) ->
-                    rgb255 100 255 100
+    column [ spacing 5 ]
+        [ Input.text
+            [ Background.color
+                (case ( touched, res ) of
+                    ( True, Ok _ ) ->
+                        rgb255 100 255 100
 
-                ( True, Err _ ) ->
-                    rgb255 255 100 100
+                    ( True, Err _ ) ->
+                        rgb255 255 100 100
 
-                _ ->
-                    rgb255 255 255 255
-            )
+                    _ ->
+                        rgb255 255 255 255
+                )
+            ]
+            { label = Input.labelHidden "TODO"
+            , text = input
+            , placeholder = Nothing
+            , onChange = msg
+            }
+        , case ( touched, res ) of
+            ( True, Err errs ) ->
+                column [ Font.size 12, spacing 5 ] (List.map (errorToString >> text) errs)
+
+            _ ->
+                none
         ]
-        { label = Input.labelHidden "TODO"
-        , text = input
-        , placeholder = Nothing
-        , onChange = msg
-        }
