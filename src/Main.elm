ccd6b55a2bb99e98abe03dd8fed56cee3e120e35
Ed Kelly <edwardjkelly@gmail.com>
2021-10-30 17:32:54 +0100

    Remove some unused code

--- src/Main.elm
+++ src/Main.elm
@@ -2,17 +2,8 @@ module Main exposing (main, myForm)
 
 import Browser
 import Element exposing (..)
-import Element.Background as Background
-import Element.Border as Border
-import Element.Input as Input
 import Field
 import Form
-import Html
-
-
-type Answer
-    = X String
-    | O String
 
 
 myForm =
@@ -63,107 +54,8 @@ update msg model =
 
 
 view model =
-    layout [padding 20] <|
+    layout [ padding 20 ] <|
         myForm.view model
-
-
-intInput : { a | input : Int, msg : number -> msg } -> Element msg
-intInput { input, msg } =
-    row
-        [ spacing 10 ]
-        [ Input.button [ Border.width 1, padding 10 ]
-            { label = text "-"
-            , onPress = Just (msg -1)
-            }
-        , text <| String.fromInt input
-        , Input.button [ Border.width 1, padding 10 ]
-            { label = text "+"
-            , onPress = Just (msg 1)
-            }
-        ]
-
-
-q label opts msg =
-    Field.custom
-        { id = ""
-        , init = Nothing
-        , msg = msg
-        , parser =
-            \opt ->
-                case opt of
-                    Just o ->
-                        if List.member o opts then
-                            Ok opt
-
-                        else
-                            Err (Field.IntTooLow 1)
-
-                    Nothing ->
-                        Ok opt
-        , renderer = questionInput opts
-        , updater = \new _ -> Just new
-        }
-        |> Field.withLabel label
-        |> Field.withValidator
-            (Maybe.andThen
-                (\opt ->
-                    case opt of
-                        O _ ->
-                            Nothing
-
-                        X _ ->
-                            Just (Field.IntTooLow 1)
-                )
-            )
-        |> Form.field
-
-
-questionInput opts { input, parsed, msg, label } =
-    column
-        [ spacing 10
-        , width fill
-        , Background.color
-            (case parsed of
-                Err _ ->
-                    rgb255 255 200 200
-
-                Ok (Just (O _)) ->
-                    rgb255 200 255 200
-
-                Ok _ ->
-                    rgb255 255 255 255
-            )
-        ]
-        [ text (label |> Maybe.withDefault "no label!")
-        , column [ spacing 5, width fill ]
-            (List.map
-                (\opt ->
-                    let
-                        optString =
-                            case opt of
-                                X x ->
-                                    x
-
-                                O o ->
-                                    o
-                    in
-                    Input.button
-                        [ Border.width 1
-                        , padding 10
-                        , width fill
-                        , Background.color
-                            (if input == Just opt || input == Nothing then
-                                rgb255 220 220 220
-
-                             else
-                                rgb255 255 255 255
-                            )
-                        ]
-                        { label = text optString, onPress = Just (msg opt) }
-                )
-                opts
-            )
-        ]
 
 
 main =
