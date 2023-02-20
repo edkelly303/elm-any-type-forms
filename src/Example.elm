76fda1e63cfd6d87a2a227484dd9d9136bb76989
Ed Kelly <edwardjkelly@gmail.com>
2023-02-20 15:26:31 +0000

    New example

--- src/Example.elm
+++ src/Example.elm
@@ -1,181 +1,71 @@
 module Example exposing (main)
 
 import Browser
-import Control exposing (..)
-import Form
-import Html as H
-import Html.Attributes as HA
-import Html.Events as HE
+import Control
 
 
 main =
     Browser.element
-        { init = init
-        , view = view
-        , update = update
+        { init = \() -> ( form.init, Cmd.none )
+        , view = form.view
+        , update = form.update
         , subscriptions = \_ -> Sub.none
         }
 
 
-init () =
-    ( mainForm.init
-    , Cmd.none
-    )
+form =
+    Control.toForm "My Lovely Form" identity exampleControl
 
 
-view model =
-    H.div
-        [ HA.style "width" "100vw"
-        , HA.style "height" "100vh"
-        , HA.style "display" "flex"
-        , HA.style "justify-content" "center"
-        , HA.style "font-family" "sans"
-        , HA.style "background-color" "gray"
-        , HA.style "margin" "-8px"
-        ]
-        [ H.div
-            [ HA.style "width" "600px"
-            , HA.style "outline" "solid 1px gray"
-            , HA.style "border-radius" "5px"
-            , HA.style "margin" "10px"
-            , HA.style "padding" "20px"
-            , HA.style "background-color" "white"
-            , HA.style "overflow" "scroll"
-            ]
-            [ mainForm.view model
-            , H.button
-                [ HA.style "margin-top" "10px"
-                , HE.onClick Submit
-                ]
-                [ H.text "Submit" ]
-            ]
-        ]
+type Example
+    = Foo String
+    | Bar { baz : String, qux : Int }
 
 
-update msg model =
-    case msg of
-        Submit ->
-            case mainForm.submit model of
-                Ok output ->
-                    let
-                        _ =
-                            Debug.log "Success" output
-                    in
-                    ( model, Cmd.none )
-
-                Err { state, errors } ->
-                    let
-                        _ =
-                            Debug.log "Errors" errors
-                    in
-                    ( state, Cmd.none )
-
-        FormMsg formMsg ->
-            mainForm.update formMsg model
-
-
-type Msg
-    = FormMsg
-        (Delta ( Delta String
-        , ( Delta String
-          , ( Delta (ListDelta String)
-            , ( Delta
-                    ( Delta ()
-                    , ( Delta
-                            ( Delta ( Delta String, ( Delta String, End ) ), End
-                            )
-                      , End
-                      )
-                    )
-              , End
-              )
-            )
-          )
-        ))
-    | Submit
-
-
-mainForm =
-    Form.fromControl "Create a User" FormMsg user
-
-
-type alias User =
-    { name : String
-    , age : Int
-    , interests : List String
-    , role : Role
-    }
-
-
-user =
-    record User
-        |> field "Name" .name (string |> onFlag "na" "Boo!")
-        |> field "Age" .age (boundedInt |> onFlag "na" "Boo!")
-        |> field "Interests" .interests (list (string |> flagIf String.isEmpty "Must not be blank"))
-        |> field "Role" .role role
-        |> end
-        |> flagIf (\{ name, age } -> name == String.fromInt age) "na"
-
-
-type Role
-    = Guest
-    | Registered Password
-
-
-role :
-    Control
-        ( State ()
-        , ( State ( State ( State String, ( State String, End ) ), End ), End )
-        )
-        ( Delta ()
-        , ( Delta ( Delta ( Delta String, ( Delta String, End ) ), End ), End )
-        )
-        Role
-role =
-    customType
-        (\guest registered tag ->
+exampleControl =
+    Control.customType
+        (\foo bar tag ->
             case tag of
-                Guest ->
-                    guest
+                Foo str ->
+                    foo str
 
-                Registered pwd ->
-                    registered pwd
+                Bar record ->
+                    bar record
         )
-        |> tag0 "Guest" Guest
-        |> tag1 "Registered" Registered password
-        |> end
+        |> Control.tag1 "Foo" Foo fooControl
+        |> Control.tag1 "Bar" Bar barControl
+        |> Control.end
+        |> Control.initWith (Bar {baz = "hello", qux = 1})
 
 
-type alias Password =
-    { password : String
-    , confirmPassword : String
-    }
+fooControl =
+    Control.string
+        |> Control.debounce 500
+        |> Control.failIf String.isEmpty "Foo must not be blank"
 
 
-password :
-    Control
-        ( State String, ( State String, End ) )
-        ( Delta String, ( Delta String, End ) )
-        Password
-password =
-    record Password
-        |> field "Enter password"
-            .password
-            (string
-                |> onFlag "password-matcher" "Must match confirm password field"
-                |> failIf (\str -> str == "") "Password can't be blank"
-            )
-        |> field "Confirm password"
-            .confirmPassword
-            (string
-                |> onFlag "password-matcher" "Must match password field"
-            )
-        |> end
-        |> flagIf (\rec -> rec.password /= rec.confirmPassword) "password-matcher"
+barControl =
+    Control.record
+        (\baz qux ->
+            { baz = baz
+            , qux = qux
+            }
+        )
+        |> Control.field "Baz" .baz bazControl
+        |> Control.field "Qux" .qux quxControl
+        |> Control.end
+        |> Control.flagIf (\record -> record.baz == String.fromInt record.qux) "baz=qux"
 
 
-boundedInt : Control String String Int
-boundedInt =
-    int
-        |> failIf (\x -> x <= 0) "must be greater than 0"
-        |> failIf (\x -> x > 120) "maximum is 120"
+bazControl =
+    Control.string
+        |> Control.debounce 500
+        |> Control.onFlag "baz=qux" "Baz must not equal Qux"
+        |> Control.failIf (String.contains "?") "Baz must not contain a '?'"
+
+
+quxControl =
+    Control.int
+        |> Control.debounce 500
+        |> Control.onFlag "baz=qux" "Qux must not equal Baz"
+        |> Control.failIf (\n -> n < 1) "Qux must greater than zero"
