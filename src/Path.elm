b48b20f962560b05af1a5c1dc7724b5ff1d8a4db
Ed Kelly <edwardjkelly@gmail.com>
2023-08-03 09:40:05 +0100

    Remove some unused functions

--- src/Path.elm
+++ src/Path.elm
@@ -1,4 +1,4 @@
-module Path exposing (Path, add, dropLast, last, root, toString)
+module Path exposing (Path, add, root, toString)
 
 
 type Path
@@ -22,14 +22,3 @@ toString (Path path) =
         |> List.map String.fromInt
         |> String.join "-"
         |> String.toLower
-
-
-last : Path -> Int
-last (Path path) =
-    List.head path
-        |> Maybe.withDefault 0
-
-
-dropLast : Path -> Path
-dropLast (Path path) =
-    Path (List.drop 1 path)
